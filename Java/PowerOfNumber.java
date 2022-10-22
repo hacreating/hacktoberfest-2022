d7d15e839990a55aac214972755921356022d0cd
ARITRA034 <115772976+ARITRA034@users.noreply.github.com>
2022-10-22 23:54:29 +0530

    Create PowerOfNumber.java

---
+++ Java/PowerOfNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class PowerOfNumber {
+    public static void main(String[] args) {
+    	
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+		int num=s.nextInt();
+		System.out.print("Enter the power: ");
+		int pow=s.nextInt();
+        //int number = 2, p = 5;
+        long result = 1;
+        
+        
+        int i = pow;
+        for (;i != 0; --i)
+        {
+            result *= num;
+        }
+        
+      
+        System.out.println(num+"^"+pow+" = "+result);
+    }
+}
