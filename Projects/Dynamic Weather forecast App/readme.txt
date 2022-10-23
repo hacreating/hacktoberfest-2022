9178d89cda755123d3d8b2fc26b0836c5bf11445
SougataCodes <103723726+SougataCodes@users.noreply.github.com>
2022-10-23 11:53:18 +0530

    Add files via upload

---
+++ Projects/Dynamic Weather forecast App/readme.txt
@@ -0,0 +1,9 @@
+This python code is to fetch real-time weather conditions for any city using Python, it takes the name of a city and returns the weather information for that city by scratching the web. 
+So instead of using a paid API, we’ll be using web scraping for this task.
+
+For this task, I have used Beautifulsoup and the requests library in Python to extract the data directly from Google. 
+The requests library comes preinstalled in the Python programming language, you need to install the Beautifulsoup library by using the pip command; 
+	*pip install beautifulsoup4
+
+
+You can use this logic for creating an advanced GUI application.
---
+++ Projects/Fetch Instagram/code.py
@@ -0,0 +1,19 @@
+# Import the module
+import instaloader
+
+# Create an instance of Instaloader class
+bot = instaloader.Instaloader()
+
+# Load a profile from an Instagram handle
+profile = instaloader.Profile.from_username(bot.context, 'amandhattarwal')
+
+print(type(profile))
+
+print("\n--------------------------------------------------------------------------------")
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers: ", profile.followers)
+print("Followees: ", profile.followees)
+print("Bio: ", profile.biography,profile.external_url)
+print("--------------------------------------------------------------------------------\n")
